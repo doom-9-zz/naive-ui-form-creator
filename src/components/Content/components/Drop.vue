75f17cf67821e88ffbb9912b6a40d4e9d59eddc3
doom-9 <429664046@qq.com>
2021-12-09 11:55:33 +0800

    feat

--- src/components/Content/components/Drop.vue
+++ src/components/Content/components/Drop.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
+import { ref, watchEffect } from 'vue';
 import { useStore } from 'vuex';
 import { getParentElementId } from '../../../utils/index';
 
 const store = useStore();
 let dragId: string | null = null;
-let dropId: string | null = null;
+const dropId = ref<string | null>(null);
+const dropContainerElement = ref<HTMLDivElement | null>(null);
+
+watchEffect(() => {
+  if (dropContainerElement.value !== null) {
+    const children = dropContainerElement.value.children;
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i] as HTMLDivElement;
+      if (child.id === dropId.value) {
+        child.style.border = '1px dotted rgb(24, 160, 88)';
+      } else {
+        child.style.border = 'none';
+      }
+    }
+  }
+});
 
 const handleDragOver = (e: DragEvent) => {
   let id = getParentElementId(e.target as HTMLElement);
-  if (id !== null && dropId !== id) {
-    dropId = id;
+  if (id !== null && dropId.value !== id) {
+    dropId.value = id;
   }
 };
 const handleDragStart = (e: DragEvent) => {
@@ -23,13 +39,20 @@ const handleDragEnd = (e: DragEvent) => {
   if (dragId !== null && dropId !== null) {
     store.commit('exchange', {
       id1: dragId,
-      id2: dropId,
+      id2: dropId.value,
     });
+    dragId = null;
+    dropId.value = null;
   }
 };
 </script>
 <template>
-  <div @dragover.prevent="handleDragOver" @dragstart="handleDragStart" @dragend="handleDragEnd">
+  <div
+    @dragover.prevent="handleDragOver"
+    @dragstart="handleDragStart"
+    @dragend="handleDragEnd"
+    ref="dropContainerElement"
+  >
     <slot></slot>
   </div>
 </template>
