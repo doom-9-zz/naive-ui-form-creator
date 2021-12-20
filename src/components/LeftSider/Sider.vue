d612be8aa72da5477ec1269f485004be7cbbec6b
doom-9 <429664046@qq.com>
2021-12-20 14:03:53 +0800

    feat

--- src/components/LeftSider/Sider.vue
+++ src/components/LeftSider/Sider.vue
@@ -22,7 +22,9 @@ const appProvideConfig = inject(appProvideKey);
 
 <template>
   <n-space vertical>
-    <n-divider title-placement="center">表单组件</n-divider>
+    <n-divider title-placement="center">
+      {{ $t('formComponent', appProvideConfig?.local.value) }}
+    </n-divider>
     <n-button
       type="primary"
       v-for="item in options"
@@ -31,14 +33,16 @@ const appProvideConfig = inject(appProvideKey);
     >
       {{ $t(item.formItemConfig.label, appProvideConfig?.local.value) }}
     </n-button>
-    <n-divider title-placement="center">辅助组件</n-divider>
+    <n-divider title-placement="center">
+      {{ $t('layoutComponent', appProvideConfig?.local.value) }}
+    </n-divider>
     <n-button
       type="warning"
       v-for="item in LayoutOptions"
       size="large"
       @click="handleLayoutAddClick(item.value)"
     >
-      {{ item.formItemConfig.label }}
+      {{ $t(item.formItemConfig.label, appProvideConfig?.local.value) }}
     </n-button>
   </n-space>
 </template>
