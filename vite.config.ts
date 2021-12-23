d5e327bc9763daabbb58cb625344ed0718b5747b
doom-9 <429664046@qq.com>
2021-12-23 11:27:50 +0800

    use unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [Components({ dts: true, resolvers: [NaiveUiResolver()] }), vue()],
+});
