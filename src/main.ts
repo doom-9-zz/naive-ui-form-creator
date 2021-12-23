f84597d0757d6cc0de91f4eba722ed7ac49d946d
doom-9 <429664046@qq.com>
2021-12-23 16:24:39 +0800

    Revert "use unplugin-vue-components"
    This reverts commit d5e327bc9763daabbb58cb625344ed0718b5747b.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import App from './App.vue';
 import { createApp } from 'vue';
+import naive from 'naive-ui';
 import { store } from './store/index';
 import i18n from 'vue3-i18n-plugin';
 import i18nConfig from './utils/i18n/const';
 
 createApp(App)
   .use(store)
+  .use(naive)
   .use(i18n, {
     initial: 'zh',
     translationConfiguration: i18nConfig,
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
-import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Components({ dts: true, resolvers: [NaiveUiResolver()] }), vue()],
-});
+  plugins: [vue()]
+})
