bacb0abab63e6920e093f26b1b2d079cd9593888
doom-9 <dddm56605@gmail.com>
2022-07-13 22:28:32 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,22 +11,22 @@ export default defineConfig({
       // options are passed on to @vue/babel-plugin-jsx
     }),
   ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      name: 'MyLib',
-      fileName: format => `my-lib.${format}.js`,
-      formats: ['es'],
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'naive-ui'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+  // build: {
+  //   lib: {
+  //     entry: resolve(__dirname, 'lib/main.ts'),
+  //     name: 'MyLib',
+  //     fileName: format => `my-lib.${format}.js`,
+  //     formats: ['es'],
+  //   },
+  //   rollupOptions: {
+  //     // 确保外部化处理那些你不想打包进库的依赖
+  //     external: ['vue', 'naive-ui'],
+  //     output: {
+  //       // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+  //       globals: {
+  //         vue: 'Vue',
+  //       },
+  //     },
+  //   },
+  // },
 })
